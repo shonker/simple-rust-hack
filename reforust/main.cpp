3a42eff1e6289d51d494db437ee2aec76d0ece3f
Emir <77904942+ReFo0@users.noreply.github.com>
2022-03-09 22:04:18 +0300

    Update main.cpp

--- reforust/main.cpp
+++ reforust/main.cpp
@@ -1,15 +1,11 @@
 #include "misc.h"
 #include "offsets.h"
 
-// I shared a Tutorial rust cheat source for you
-// This resource is just an instructional resource if you have knowledge you can really benefit from this resource
-// Contact: ReFo#7149
-
 int main() {
 	auto process = get_process_id("RustClient.exe");
 	if (process) {
 		 auto process_base_addr = get_base_address("RustClient.exe");
-		 auto assembly_base_addr = get_base_address("GameAssembly.dll"); // We have nothing to do with UnityPlayer.dll because it will only have adminmode feature
+		 auto assembly_base_addr = get_base_address("GameAssembly.dll"); 
 			auto localplayer1 = memory::read<uintptr_t>(assembly_base_addr + basenetworkable);
 			auto localplayer2 = memory::read<uintptr_t>(localplayer1 + 0xB8);
 			auto localplayer3 = memory::read<uintptr_t>(localplayer2 + 0x0);
