760c568f32522dabf10d94075ddb4a2635d7bdcf
Emir <77904942+ReFo0@users.noreply.github.com>
2022-03-09 22:03:52 +0300

    Update main.cpp

--- reforust/main.cpp
+++ reforust/main.cpp
@@ -10,13 +10,13 @@ int main() {
 	if (process) {
 		 auto process_base_addr = get_base_address("RustClient.exe");
 		 auto assembly_base_addr = get_base_address("GameAssembly.dll"); // We have nothing to do with UnityPlayer.dll because it will only have adminmode feature
-			DWORD64 localplayer1 = memory::read<DWORD64>(assembly_base_addr + basenetworkable);
-			DWORD64 localplayer2 = memory::read<DWORD64>(localplayer1 + 0xB8);
-			DWORD64 localplayer3 = memory::read<DWORD64>(localplayer2 + 0x0);
-			DWORD64 localplayer4 = memory::read<DWORD64>(localplayer3 + 0x10);
-			DWORD64 localplayer5 = memory::read<DWORD64>(localplayer4 + 0x28);
-			DWORD64 localplayer6 = memory::read<DWORD64>(localplayer5 + 0x18);
-			DWORD64 localplayer7 = memory::read<DWORD64>(localplayer6 + 0x20);
+			auto localplayer1 = memory::read<uintptr_t>(assembly_base_addr + basenetworkable);
+			auto localplayer2 = memory::read<uintptr_t>(localplayer1 + 0xB8);
+			auto localplayer3 = memory::read<uintptr_t>(localplayer2 + 0x0);
+			auto localplayer4 = memory::read<uintptr_t>(localplayer3 + 0x10);
+			auto localplayer5 = memory::read<uintptr_t>(localplayer4 + 0x28);
+			auto localplayer6 = memory::read<uintptr_t>(localplayer5 + 0x18);
+			auto localplayer7 = memory::read<uintptr_t>(localplayer6 + 0x20);
 		        while (true) {
 			int flag = memory::read<int>(localplayer7 + playerflags);
 			memory::write<int>(playerflags, (flag |= admin));
