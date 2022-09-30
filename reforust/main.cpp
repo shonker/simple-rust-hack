93c1cda3d8a4d057309ad43a9626b45a11db0b3e
Emir <77904942+ReFo0@users.noreply.github.com>
2022-09-30 17:53:34 +0300

    Update main.cpp

--- reforust/main.cpp
+++ reforust/main.cpp
@@ -8,13 +8,13 @@ int main()
 	auto process = get_process_id("RustClient.exe");
 	if (process) {
 		auto assembly_base_address = get_base_address("GameAssembly.dll");
-		auto assembly_base_offsets = memory::read<uintptr_t>(assembly_base_address + 0x3171be0);
-		auto assembly_main_offsets = memory::read_chain<uintptr_t>(assembly_base_offsets, { 0xb8, 0x0, 0x10, 0x28, 0x18, 0x20 });
+		auto assembly_base_offsets = memory::read<uintptr_t>(assembly_base_address + 56564432);
+		auto assembly_main_offsets = memory::read_chain<uintptr_t>(assembly_base_offsets, { 0xb8, 0x0, 0x10, 0x28 });
 		while (true)
 		{
 			if (adminmode) {
-				int flag = memory::read<int>(assembly_main_offsets + 0x678);
-				memory::write<int>(0x678, (flag |= 4));
+				int flag = memory::read<int>(assembly_main_offsets + 0x690);
+				memory::write<int>(0x690, (flag |= 4));
 			}
 		}
 	}
